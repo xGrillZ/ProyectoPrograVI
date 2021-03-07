70c4abbd24ffff4f98be1e66731544ae63c87b30
Steven Vargas <72053895+xGrillZ@users.noreply.github.com>
2021-03-07 02:00:30 -0600

    Creación del diseño página de logeo y responsive

---
+++ SistVehiculo/SistVehiculo/Controllers/InicioController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SistVehiculo.Models;
+
+namespace SistVehiculo.Controllers
+{
+    public class InicioController : Controller
+    {
+        /// <summary>
+        /// Instancia del modelo de base de datos
+        /// </summary>
+        sistVehiculoEntities modeloBD = new sistVehiculoEntities();
+
+        // GET: Inicio
+        public ActionResult Inicio()
+        {
+            return View();
+        }
+    }
+}
