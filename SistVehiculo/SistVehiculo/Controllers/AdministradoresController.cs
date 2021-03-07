4fc12965a446927302201b34fe7cf21d32165798
RANDALL\randall <randallab_@outlook.com>
2021-03-07 15:29:11 -0600

    Add Mantenimientos Views
    Add Mantenimientos Views

--- SistVehiculo/SistVehiculo/Controllers/AdministradoresController.cs
+++ SistVehiculo/SistVehiculo/Controllers/AdministradoresController.cs
@@ -3,15 +3,129 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SistVehiculo.Models;
 
 namespace SistVehiculo.Controllers
 {
     public class AdministradoresController : Controller
     {
+        sistVehiculoEntities modeloBD = new sistVehiculoEntities();
         // GET: Administradores
-        public ActionResult Index()
+
+        public ActionResult ListaServiciosProductos()
         {
             return View();
         }
+        public ActionResult InsertarServiciosProductos()
+        {
+            return View();
+        }
+
+        public ActionResult ModificarServiciosProductos()
+        {
+            return View();
+        }
+
+        public ActionResult EliminarServiciosProductos()
+        {
+            return View();
+        }
+
+        public ActionResult ListaTipoVehiculos()
+        {
+            return View();
+        }
+        public ActionResult InsertarTipoVehiculos()
+        {
+            return View();
+        }
+
+        public ActionResult ModificarTipoVehiculos()
+        {
+            return View();
+        }
+
+        public ActionResult EliminarTipoVehiculos()
+        {
+            return View();
+        }
+
+        public ActionResult ListaMarcaVehiculos()
+        {
+            return View();
+        }
+        public ActionResult InsertarMarcaVehiculos()
+        {
+            return View();
+        }
+
+        public ActionResult ModificarMarcaVehiculos()
+        {
+            return View();
+        }
+
+        public ActionResult EliminarMarcaVehiculos()
+        {
+            return View();
+        }
+
+        public ActionResult ListaClientes()
+        {
+            return View();
+        }
+        public ActionResult InsertarClientes()
+        {
+            return View();
+        }
+
+        public ActionResult ModificarClientes()
+        {
+            return View();
+        }
+
+        public ActionResult EliminarClientes()
+        {
+            return View();
+        }
+
+        public ActionResult ListaVehiculos()
+        {
+            return View();
+        }
+        public ActionResult InsertarVehiculos()
+        {
+            return View();
+        }
+
+        public ActionResult ModificarVehiculos()
+        {
+            return View();
+        }
+
+        public ActionResult EliminarVehiculos()
+        {
+            return View();
+        }
+
+        public ActionResult ListaVehiculosCliente()
+        {
+            return View();
+        }
+        public ActionResult InsertarVehiculosCliente()
+        {
+            return View();
+        }
+
+        public ActionResult ModificarVehiculosCliente()
+        {
+            return View();
+        }
+
+        public ActionResult EliminarVehiculosCliente()
+        {
+            return View();
+        }
+
+
     }
 }
--- SistVehiculo/SistVehiculo/Controllers/ConsultoresController.cs
+++ SistVehiculo/SistVehiculo/Controllers/ConsultoresController.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SistVehiculo.Models;
 
 namespace SistVehiculo.Controllers
 {
     public class ConsultoresController : Controller
     {
+        sistVehiculoEntities modeloBD = new sistVehiculoEntities();
         // GET: Consultores
-        public ActionResult Index()
+
+        public ActionResult ListaClientes()
+        {
+            return View();
+        }
+        public ActionResult ModificarClientes()
         {
             return View();
         }
